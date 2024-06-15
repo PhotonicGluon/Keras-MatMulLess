021a8a689e4523c5087f8cf4b8301dcd617ef258
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-15 13:26:19 +0800

    Create `run_tests.yml` GitHub action
    Also fixed `test_misc.py` to use the correct array conversion method

--- tests/layers/test_misc.py
+++ tests/layers/test_misc.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pytest
 
+from keras_mml.utils import as_numpy
+
 
 def test_simple_rms_norm():
     from keras_mml.layers.misc.rms_norm import RMSNorm
 
     x = np.array([1, 2, 3])
     y = RMSNorm(2)(x)
-    y_pred = y.numpy()
+    y_pred = as_numpy(y)
     y_true = [0.18898223, 0.37796447, 0.5669467]
 
     for i in range(len(y_pred)):
@@ -15,7 +17,7 @@ def test_simple_rms_norm():
 
     x = np.array([4, 5, 6, 7])
     y = RMSNorm(5)(x)
-    y_pred = y.numpy()
+    y_pred = as_numpy(y)
     print(y_pred)
     y_true = [0.15936382, 0.19920477, 0.23904571, 0.27888668]
 
