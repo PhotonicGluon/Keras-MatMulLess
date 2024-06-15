a89992e6f6b6a306acbbe4904baf63337fedcd7c
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-15 13:22:37 +0800

    Create `as_numpy()` utility method

---
+++ keras_mml/utils/array.py
@@ -0,0 +1,28 @@
+"""
+Utilities for handing numpy arrays.
+"""
+
+from typing import Any
+import numpy as np
+
+
+def as_numpy(x: Any) -> np.ndarray:
+    """
+    Converts the given object into a numpy array.
+
+    This function will first attempt to call the ``.numpy()`` method of the object and return it.
+    Failing to do so, it will revert to the :external:py:func:`numpy.array()` function.
+
+    Args:
+        x: Object to convert into a numpy array.
+
+    Returns:
+        Numpy array of the given object.
+    """
+
+    try:
+        return x.numpy()
+    except AttributeError:
+        pass
+
+    return np.array(x)
---
+++ tests/utils/test_array.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pytest
+from keras_mml.utils import as_numpy
+
+
+def test_tf():
+    try:
+        import tensorflow as tf
+    except ModuleNotFoundError:
+        pytest.skip("Tensorflow not installed")
+
+    orig = np.array([1, 2, 3])
+    converted = tf.convert_to_tensor(orig)
+    reverted = as_numpy(converted)
+
+    assert np.array_equal(orig, reverted)
+
+
+def test_pytorch():
+    try:
+        import torch
+    except ModuleNotFoundError:
+        pytest.skip("PyTorch not installed")
+
+    orig = np.array([1, 2, 3])
+    converted = torch.tensor(orig)
+    reverted = as_numpy(converted)
+
+    assert np.array_equal(orig, reverted)
+
+
+def test_jax():
+    try:
+        import jax.numpy as jnp
+    except ModuleNotFoundError:
+        pytest.skip("Jax not installed")
+
+    orig = np.array([1, 2, 3])
+    converted = jnp.array(orig)
+    reverted = as_numpy(converted)
+
+    assert np.array_equal(orig, reverted)
