96262fdc5b3d874cb970c83e8da7e22e6d39a86b
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-19 21:28:56 +0800

    Update gru.py

--- keras_mml/layers/gru.py
+++ keras_mml/layers/gru.py
@@ -9,4 +9,6 @@ import keras
 class GRUMML(keras.Layer):
     """
     TODO: ADD DOCS
+
+    Look at https://github.com/ridgerchu/matmulfreellm/blob/4a27497acfaa95fd7e88dad720ef8782f695b0f0/mmfreelm/layers/hgrn_bit.py#L22, https://arxiv.org/pdf/2406.02528 section 3.3.1, https://arxiv.org/pdf/2311.04823 section 3.2.
     """
