6e1c502ba853fb167a7de278c1baf99861e6bedb
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-20 22:09:07 +0800

    Modify `DenseMML` saving and loading methods

--- keras_mml/layers/dense.py
+++ keras_mml/layers/dense.py
@@ -33,6 +33,9 @@ class DenseMML(keras.Layer):
     must be of the form ``(batch_size, d0)``. An input shape that does not conform to this will
     raise a :py:exc:`ValueError`.
 
+    .. WARNING::
+       Once a model that uses this layer is loaded from a file, it **cannot** be retrained.
+
     Attributes:
         units: Dimensionality of the output space.
         activation: Activation function.
@@ -72,7 +75,7 @@ class DenseMML(keras.Layer):
         self.activation = activations.get(activation)
         self.weights_initializer = initializers.get(weights_initializer)
 
-        self._beta = None  # Used for when the layer is loaded from file
+        self._weight_scale = None  # Used for when the layer is loaded from file
 
     # Helper methods
     @staticmethod
@@ -147,14 +150,19 @@ class DenseMML(keras.Layer):
         input_dim = ops.shape(inputs)[1]
         x_norm = RMSNorm(input_dim)(inputs)
 
-        x_quantized, gamma_qb = self._activations_quantization(x_norm)
-        w_quantized, beta = self._weights_quantization(self.w)
+        # Then get the quantized activations and weights
+        x_quantized, x_scale = self._activations_quantization(x_norm)
 
-        if self._beta is not None:  # Using a saved layer
-            # FIXME: This doesn't work for saved layers
-            beta = self._beta
+        if self._weight_scale:
+            # Weights should have been pre-quantized
+            w_quantized, w_scale = self.w, self._weight_scale
+        else:
+            w_quantized, w_scale = self._weights_quantization(self.w)
 
-        scaling = beta * gamma_qb
+        scaling = w_scale * x_scale
+
+        # Perform kernel operation
+        # TODO: Make this more efficient when we are doing inference only
         x = ops.matmul(x_quantized, w_quantized) / scaling  # The `matmul` should just involve addition and subtraction
 
         # Then apply activation
@@ -203,20 +211,12 @@ class DenseMML(keras.Layer):
             store: Dictionary where the state of the model will be saved.
         """
 
-        all_vars = self._trainable_variables + self._non_trainable_variables
-        for i, v in enumerate(all_vars):
-            # FIXME: Fix this
-            # if v.name == WEIGHTS_NAME:
-            #     # Pre-quantize the weights
-            #     w_quantized, beta = self._weights_quantization(v)
+        # Pre-quantize the weights
+        w_quantized, beta = self._weights_quantization(self.w)
 
-            #     # For more efficient saving, convert the weights to boolean values, where 1 is True and -1 (and 0) are
-            #     # False
-            #     w_bools = w_quantized > 0
-            #     store[f"{i}"] = w_bools
-            #     store[f"{i}-beta"] = beta
-            # else:  # pragma: no cover
-            store[f"{i}"] = v
+        # TODO: Develop more efficient weight saving method than this simple method
+        store["weights"] = w_quantized
+        store["scale"] = beta
 
     def load_own_variables(self, store: Dict):
         """
@@ -226,61 +226,12 @@ class DenseMML(keras.Layer):
             store: Dictionary from which the state of the model will be loaded.
 
         Raises:
-            ValueError: If the layer was never built but the weights file lists variables for the
-                layer.
-            ValueError: If the expected number of variables for the layer does not match the number
-                of variables provided by the weights file.
+            ValueError: If the layer is missing variables when loading from a file.
         """
 
-        all_vars = self._trainable_variables + self._non_trainable_variables
-        expected_count = len(all_vars)
-        # for v in all_vars:
-        #     if v.name == WEIGHTS_NAME:
-        #         expected_count += 1
-
-        if len(store.keys()) != expected_count:
-            if expected_count == 0 and not self.built:  # pragma: no cover
-                raise ValueError(
-                    f"Layer '{self.name}' was never built and thus it doesn't have any variables. "
-                    f"However the weights file lists {len(store.keys())} "
-                    "variables for this layer.\n"
-                    "In most cases, this error indicates that either:\n\n"
-                    "1. The layer is owned by a parent layer that "
-                    "implements a `build()` method, but calling the "
-                    "parent's `build()` method did NOT create the state of "
-                    f"the child layer '{self.name}'. A `build()` method "
-                    "must create ALL state for the layer, including "
-                    "the state of any children layers.\n\n"
-                    "2. You need to implement "
-                    "the `def build_from_config(self, config)` method "
-                    f"on layer '{self.name}', to specify how to rebuild "
-                    "it during loading. "
-                    "In this case, you might also want to implement the "
-                    "method that generates the build config at saving time, "
-                    "`def get_build_config(self)`. "
-                    "The method `build_from_config()` is meant "
-                    "to create the state "
-                    "of the layer (i.e. its variables) upon deserialization.",
-                )
-            raise ValueError(  # pragma: no cover
-                f"Layer '{self.name}' expected {expected_count} variables, "
-                "but received "
-                f"{len(store.keys())} variables during loading. "
-                f"Expected: {[v.name for v in all_vars]}"
-            )
-
-        for i, v in enumerate(all_vars):
-            # if v.name == WEIGHTS_NAME:
-            #     # Get the quantized weights and the scale
-            #     w_quantized = store[f"{i}"][()]  # These are HDF5 dataset objects
-            #     beta = store[f"{i}-beta"][()]
-
-            #     # Pre-multiply them to get back the correct weights
-            #     w = w_quantized / beta
-            #     v.assign(w)
-
-            #     # Update the beta value to signal that we are using a saved weight
-            #     self._beta = beta
-
-            # else:  # pragma: no cover
-            v.assign(store[f"{i}"])
+        # TODO: Develop more efficient weight loading method than this simple method
+        try:
+            self.w = store["weights"][()]
+            self._weight_scale = store["scale"][()]
+        except ValueError:
+            raise ValueError("DenseMML layer missing values when loading from file")
