3ecc9ab510766d9e3cb1eeb7d9888802710c1e2a
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-21 18:50:12 +0800

    Use new precision saving approach

--- keras_mml/layers/dense/torch_dense.py
+++ keras_mml/layers/dense/torch_dense.py
@@ -12,20 +12,18 @@ class TorchDenseMML(BaseDenseMML):
     # TODO: ADD DOCS
     def _call(self, x_norm: torch.Tensor) -> torch.Tensor:
         # Get the quantized activations and weights
-        x_quantized, x_scale = self._activations_quantization(x_norm)
-        x_quantized = x_norm + (x_quantized - x_norm).detach()  # STE trick using torch detach
+        x_quantized = (
+            x_norm + (self._activations_quantization(x_norm) - x_norm).detach()
+        )  # STE trick using torch detach
 
         if self._weight_scale:
             # Weights should have been pre-quantized
-            w_quantized, w_scale = self.w, self._weight_scale
+            w_quantized = self.w / self._weight_scale
         else:
-            w_quantized, w_scale = self._weights_quantization(self.w)
-            w_quantized = self.w + (w_quantized - self.w).detach()
-
-        scaling = w_scale * x_scale
+            w_quantized = self.w + (self._weights_quantization(self.w) - self.w).detach()
 
         # Perform kernel operation
         # TODO: Make this more efficient when we are doing inference only
         # FIXME: This little transposing maneuver's gonna cost us 51 years
-        y = F.linear(x_quantized, w_quantized.T) / scaling  # The `matmul` should just involve addition and subtraction
+        y = F.linear(x_quantized, w_quantized.T)  # The `matmul` should just involve addition and subtraction
         return y
