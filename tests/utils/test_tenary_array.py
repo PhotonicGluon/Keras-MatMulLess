7d4194d1a18ba2b56be070db0fe92a24919514c5
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-21 11:51:14 +0800

    Fix bugs in ternary array encoding/decoding

--- tests/utils/test_tenary_array.py
+++ tests/utils/test_tenary_array.py
@@ -19,7 +19,7 @@ def test_encode_ternary_array():
     # Abnormal inputs
     with pytest.raises(ValueError):
         encode_ternary_array(np.array([1, 1, 1]))
-    
+
     with pytest.raises(ValueError):
         encode_ternary_array(np.array([[[1, 0, 0]]]))
 
@@ -28,13 +28,29 @@ def test_decode_ternary_array():
     # Normal inputs
     x = decode_ternary_array((4, 3), b"\xab\x49\x58")
     assert np.array_equal(x, np.array([[1, 1, 1], [-1, 0, 1], [0, 1, 0], [1, 1, -1]]))
-    
+
     x = decode_ternary_array((3, 4), b"\xab\x49\x58")
     assert np.array_equal(x, np.array([[1, 1, 1, -1], [0, 1, 0, 1], [0, 1, 1, -1]]))
-    
+
     # Abnormal inputs
     with pytest.raises(ValueError):
         decode_ternary_array((1,), b"\xab\x49\x58")
-        
+
     with pytest.raises(ValueError):
         decode_ternary_array((3, 4), b"")
+
+    # Other tests
+    x = np.array([[0, 0, 0, 0]])
+    assert np.array_equal(x, decode_ternary_array(*encode_ternary_array(x)))
+
+    x = np.array([[1, 1, 1, 1]])
+    assert np.array_equal(x, decode_ternary_array(*encode_ternary_array(x)))
+
+    x = np.array([[-1, -1, -1, -1]])
+    assert np.array_equal(x, decode_ternary_array(*encode_ternary_array(x)))
+
+    x = np.array([[0, 1, 1, 1, 1]])
+    assert np.array_equal(x, decode_ternary_array(*encode_ternary_array(x)))
+
+    x = np.array([[0, -1, -1, -1, -1]])
+    assert np.array_equal(x, decode_ternary_array(*encode_ternary_array(x)))
