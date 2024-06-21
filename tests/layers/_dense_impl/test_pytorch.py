9890b309d508ae3006ffa352acfd471947f00eb4
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-21 21:24:00 +0800

    Add tests for backend-dependent `DenseMML`

---
+++ tests/layers/_dense_impl/test_pytorch.py
@@ -0,0 +1,26 @@
+import os
+
+import numpy as np
+import pytest
+
+try:
+    import torch
+
+    os.environ["KERAS_BACKEND"] = "torch"
+except ModuleNotFoundError:
+    pytest.skip("PyTorch not installed; skipping", allow_module_level=True)
+
+from keras_mml.layers._dense_impl.torch_dense import TorchDenseMML
+from keras_mml.layers.dense import DenseMML
+from keras_mml.utils.array import as_numpy
+from tests.layers._dense_impl.setup import frontend_answer, weights
+
+
+def test_check_implementation_consistency(weights):
+    assert TorchDenseMML in DenseMML.__mro__
+
+    backend_layer = TorchDenseMML()
+    backend_answer = backend_layer._weights_quantization(torch.tensor(weights))
+    backend_answer = as_numpy(backend_answer)
+
+    assert np.allclose(backend_answer, frontend_answer(weights, DenseMML))
