ff77cc96d061ff001e637857142f7f2c7e57ad1a
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-22 14:20:45 +0800

    Allow `DenseMML` to take in any input shape
    Except one with rank 1, of course

--- keras_mml/layers/dense.py
+++ keras_mml/layers/dense.py
@@ -30,11 +30,19 @@ class DenseMML(keras.Layer):
         \\mathbf{y} = \\sigma\\left(\\mathbf{x}\\mathbf{W}^\\intercal + \\mathbf{b}\\right)
 
     where :math:`\\mathbf{x}` is the quantized input vector, :math:`\\mathbf{W}` is the quantized
-    weights matrix, :math:`\\mathbf{b}` is the bias vector, and :math:`\\sigma` is the element-wise
-    activation function.
+    weights matrix (i.e., the *kernel* matrix), :math:`\\mathbf{b}` is the bias vector, and
+    :math:`\\sigma` is the element-wise activation function.
 
-    This implementation only allows the layer to have a rank of 2. That is, the input to this layer
-    must be of the form ``(batch_size, d0)``.
+    .. NOTE::
+       If the input to the layer (say :math:`\\mathbf{x}`) has a rank greater than 2, then this
+       computes the dot product of :math:`\\mathbf{x}` and :math:`\\mathbf{W}` along the last axis
+       of the :math:`\\mathbf{x}` and axis ``0`` of :math:`\\mathbf{W}`.
+
+       For example, suppose :math:`\\mathbf{x}` has shape ``(batch_size, d0, d1)``. Then
+       :math:`\\mathbf{W}` is created to have shape ``(d1, units)`` and it operates along axis ``2``
+       of :math:`\\mathbf{x}` on every sub-tensor of shape ``(1, 1, d1)`` (there are
+       ``batch_size * d0`` such sub-tensors). The output in this case will have shape
+       ``(batch_size, d0, units)``.
 
     .. WARNING::
        Once a model that uses this layer is loaded from a file, it **cannot** be retrained.
@@ -42,7 +50,7 @@ class DenseMML(keras.Layer):
     Attributes:
         units: Dimensionality of the output space.
         use_bias: Whether the layer uses a bias vector.
-        kernel_initializer: Initializer for the weights matrix.
+        kernel_initializer: Initializer for the kernel matrix.
         bias_initializer: Initializer for the bias vector.
 
     .. |1.58 Bit LLMs| replace:: *The Era of 1-bit LLMs: All Large Language Models are in 1.58 Bits*
@@ -66,7 +74,7 @@ class DenseMML(keras.Layer):
             activation: Activation function to use. If you don't specify anything, no activation is
                 applied (i.e. "linear" activation: :math:`\\sigma(\\mathbf{x}) = \\mathbf{x}`).
             use_bias: Whether the layer uses a bias vector.
-            kernel_initializer: Initializer for the weights matrix.
+            kernel_initializer: Initializer for the kernel matrix.
             bias_initializer: Initializer for the bias vector.
             **kwargs: Keyword arguments for :py:class:`keras.Layer`.
 
@@ -81,15 +89,13 @@ class DenseMML(keras.Layer):
                 f"Received an invalid value for argument `units`, expected a positive integer, got {units}"
             )
 
-        self.input_spec = keras.layers.InputSpec(ndim=2)
-
         self.units = units
         self.activation = activations.get(activation)
         self.use_bias = use_bias
         self.kernel_initializer = initializers.get(kernel_initializer)
         self.bias_initializer = initializers.get(bias_initializer)
 
-        self._weight_scale = None  # Used for when the layer is loaded from file
+        self._kernel_scale = None  # Used for when the layer is loaded from file
 
     # Helper methods
     @staticmethod
@@ -109,18 +115,18 @@ class DenseMML(keras.Layer):
         return y
 
     @staticmethod
-    def _weights_quantization(w, for_saving: bool = False) -> Union[Any, Tuple[Any, float]]:
+    def _kernel_quantization(w, for_saving: bool = False) -> Union[Any, Tuple[Any, float]]:
         """
-        Quantizes the weights to 1.58 bits (i.e., :math:`\\log_{2}3` bits).
+        Quantizes the kernel values to 1.58 bits (i.e., :math:`\\log_{2}3` bits).
 
         Args:
-            w: Array of weights.
+            w: Kernel matrix.
             for_saving: Whether this should output values in preparation for saving.
 
         Returns:
-            The quantized weights with the scaling applied. If the quantization is for saving, then
-            both the quantized weights and the scale will be returned, with the scale **not**
-            applied to the quantized weights.
+            The quantized kernel with the scaling applied. If the quantization is for saving, then
+            both the quantized kernel and the scale will be returned, with the scale **not**
+            applied to the quantized kernel.
         """
 
         scale = 1.0 / ops.clip(ops.mean(ops.abs(w)), EPSILON, HUGE)
@@ -132,55 +138,54 @@ class DenseMML(keras.Layer):
 
     def _get_quantized_arrays(self, x_norm) -> Tuple[Any, Any]:
         """
-        Gets the quantized activation and weight values.
+        Gets the quantized activation and kernel values.
 
         Args:
             x_norm: Normalized activation values.
 
         Returns:
             A tuple. The first value is the quantized activation values. The second is the quantized
-            weight values.
+            kernel values.
         """
 
-        # Get the quantized activations and weights
+        # Get the quantized activations and kernel
         # (We use a Straight-Through Estimator (STE) trick by stopping gradient propagation)
         x_quantized = x_norm + ops.stop_gradient(self._activations_quantization(x_norm) - x_norm)
 
-        if self._weight_scale:
-            # Weights should have been pre-quantized
-            w_quantized = self.w / self._weight_scale
+        if self._kernel_scale:
+            # Kernel values should have been pre-quantized
+            w_quantized = self._kernel / self._kernel_scale
         else:
-            w = self.w
-            w_quantized = w + ops.stop_gradient(self._weights_quantization(w) - w)
+            w = self._kernel
+            w_quantized = w + ops.stop_gradient(self._kernel_quantization(w) - w)
 
         return x_quantized, w_quantized
 
     # Public methods
-    def build(self, input_shape: Tuple[int, int]):
+    def build(self, input_shape: Tuple[int, ...]):
         """
         Create layer weights.
 
         Args:
             input_shape: Shape of the input.
-
-        Raises:
-            ValueError: If the input shape does not have a rank of 2 (i.e., something like
-                ``(batch_size, d0)``).
         """
 
-        self.w = self.add_weight(
-            name="weights",
-            shape=(input_shape[-1], self.units),
+        input_dim = input_shape[-1]
+
+        self._kernel = self.add_weight(
+            name="kernel",
+            shape=(input_dim, self.units),
             initializer=self.kernel_initializer,
             trainable=True,
         )
         if self.use_bias:
-            self.bias = self.add_weight(
+            self._bias = self.add_weight(
                 name="bias", shape=(self.units,), initializer=self.bias_initializer, trainable=True
             )
         else:
-            self.bias = None
+            self._bias = None
 
+        self.input_spec = keras.layers.InputSpec(min_ndim=2, axes={-1: input_dim})
         self.built = True
 
     def call(self, inputs):
@@ -206,13 +211,13 @@ class DenseMML(keras.Layer):
         x = ops.matmul(x_quantized, w_quantized)  # The `matmul` should just involve addition and subtraction
 
         # Then apply bias and activation
-        if self.bias is not None:
-            x = ops.add(x, self.bias)
+        if self._bias is not None:
+            x = ops.add(x, self._bias)
         if self.activation is not None:
             x = self.activation(x)
         return x
 
-    def compute_output_shape(self, input_shape: Tuple[int, int]) -> Tuple[int, int]:
+    def compute_output_shape(self, input_shape: Tuple[int, ...]) -> Tuple[int, ...]:
         """
         Computes the output shape given a tensor of a given shape.
 
@@ -235,17 +240,18 @@ class DenseMML(keras.Layer):
             store: Dictionary where the state of the model will be saved.
         """
 
-        # Pre-quantize the weights
-        w_quantized, w_scale = self._weights_quantization(self.w, for_saving=True)
+        # Pre-quantize the kernel values
+        w_quantized, w_scale = self._kernel_quantization(self._kernel, for_saving=True)
 
-        # Encode the ternary weights efficiently
+        # Encode the ternary array efficiently
         shape, encoded = encode_ternary_array(as_numpy(w_quantized))
 
         # Then store the variables
         store["kernel_encoded"] = np.frombuffer(encoded, dtype="uint8")
         store["kernel_shape"] = shape
         store["kernel_scale"] = w_scale
-        store["bias"] = self.bias
+
+        store["bias"] = self._bias
 
     def load_own_variables(self, store: Dict):
         """
@@ -263,12 +269,13 @@ class DenseMML(keras.Layer):
             encoded = store["kernel_encoded"][()].tobytes()
             shape = store["kernel_shape"][()]
             scale = store["kernel_scale"][()]
+
             bias = store["bias"][()]
         except ValueError:  # pragma: no cover
             raise ValueError("DenseMML layer missing values when loading from file")
 
         # Then recover the weights
-        self.w.assign(decode_ternary_array(shape, encoded))
-        self._weight_scale = scale
+        self._kernel.assign(decode_ternary_array(shape, encoded))
+        self._kernel_scale = scale
 
-        self.bias = bias
+        self._bias = bias
