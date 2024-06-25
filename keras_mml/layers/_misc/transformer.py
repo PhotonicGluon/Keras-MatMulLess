d6da5479f0b24da123a34b63d40bf2fb1b6f3d4f
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-25 16:25:16 +0800

    Add initial implementation of `TransformerMML`

--- keras_mml/layers/_misc/transformer.py
+++ keras_mml/layers/_misc/transformer.py
@@ -2,7 +2,13 @@
 Implements a matmul-less transformer block.
 """
 
+from typing import Optional
+
 import keras
+
+from keras_mml.layers._activations import SwiGLUMML
+from keras_mml.layers._normalizations.rms_norm import RMSNorm
+from keras_mml.layers._recurrent.gru import GRUMML
 
 
 @keras.saving.register_keras_serializable(package="keras_mml")
@@ -10,3 +16,49 @@ class TransformerBlockMML(keras.Layer):
     """
     TODO: ADD
     """
+
+    def __init__(
+        self,
+        hidden_size: int = 2048,
+        hidden_ratio: int = 4,
+        rate: float = 0.1,
+        intermediate_size: Optional[int] = None,
+        activation: str = "sigmoid",
+        **kwargs
+    ):  # TODO: Add
+        """
+        TODO: ADD
+        """
+
+        super().__init__(**kwargs)
+
+        self.hidden_size = hidden_size
+
+        self.attention = GRUMML(hidden_size, fully_mml=True)
+        self.attention_dropout = keras.layers.Dropout(rate)
+        self.attention_norm = RMSNorm()
+
+        self.ffn = SwiGLUMML(
+            hidden_size, hidden_ratio=hidden_ratio, intermediate_size=intermediate_size, activation=activation
+        )
+        self.ffn_dropout = keras.layers.Dropout(rate)
+        self.ffn_norm = RMSNorm()
+
+    def call(self, inputs):
+        """
+        Calling method of the layer.
+
+        Args:
+            inputs: Inputs into the layer.
+
+        Returns:
+            Transformed inputs.
+        """
+
+        attn_output = self.attention(inputs, inputs)
+        attn_output = self.attention_dropout(attn_output)
+        attn_output = self.attention_norm(inputs + attn_output)
+
+        ffn_output = self.ffn(attn_output)
+        ffn_output = self.ffn_dropout(ffn_output)
+        return self.ffn_norm(attn_output + ffn_output)
