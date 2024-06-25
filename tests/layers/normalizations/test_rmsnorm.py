7132f6fc00d5cf0b1b2a18e27128f0f29a04de4d
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-06-25 15:56:08 +0800

    Rewrite tests for `RMSNorm`

--- tests/layers/normalizations/test_rmsnorm.py
+++ tests/layers/normalizations/test_rmsnorm.py
@@ -1,25 +1,45 @@
 import numpy as np
-import pytest
+from keras import ops
 
 from keras_mml.layers import RMSNorm
 from keras_mml.utils import as_numpy
 
 
-def test_rms_norm():
+def test_no_learnable_weights():
     x = np.array([1, 2, 3])
-    y = RMSNorm(2)(x)
+    y = RMSNorm(has_learnable_weights=False)(x)
     y_pred = as_numpy(y)
-    y_true = np.array([0.18898223, 0.37796447, 0.5669467])
+    y_true = np.array([0.46291004, 0.92582010, 1.38873015])
     assert np.allclose(y_pred, y_true)
 
-    x = np.array([4, 5, 6, 7])
-    y = RMSNorm(5)(x)
+    x = np.array([4, -5, 6, -7])
+    y = RMSNorm(has_learnable_weights=False)(x)
     y_pred = as_numpy(y)
-    y_true = [0.15936382, 0.19920477, 0.23904571, 0.27888668]
+    y_true = [0.71269665, -0.89087081, 1.06904497, -1.24721913]
     assert np.allclose(y_pred, y_true)
 
-    with pytest.raises(ValueError):
-        RMSNorm(0)
 
-    with pytest.raises(ValueError):
-        RMSNorm(-1)
+def test_with_learnable_weights():
+    # 1D array
+    x = np.array([1, 2, 3])
+    y = RMSNorm(use_bias=False)(x)
+    assert ops.shape(y) == ops.shape(x)
+
+    y = RMSNorm(use_bias=True)(x)
+    assert ops.shape(y) == ops.shape(x)
+
+    # 2D array
+    x = np.array([[1, 2, 3], [4, 5, 6]])
+    y = RMSNorm(use_bias=False)(x)
+    assert ops.shape(y) == ops.shape(x)
+
+    y = RMSNorm(use_bias=True)(x)
+    assert ops.shape(y) == ops.shape(x)
+
+    # 3D array
+    x = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+    y = RMSNorm(use_bias=False)(x)
+    assert ops.shape(y) == ops.shape(x)
+
+    y = RMSNorm(use_bias=True)(x)
+    assert ops.shape(y) == ops.shape(x)
