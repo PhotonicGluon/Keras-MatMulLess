d51ae2f9db0d3e5bb857e7146a538d48c1c0b266
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-07-01 16:26:21 +0800

    Create PyTorch algorithm to flatten stacked matrix tensor

--- tests/utils/array/test_ternary_multiplication.py
+++ tests/utils/array/test_ternary_multiplication.py
@@ -1,44 +1,54 @@
+import keras
 import numpy as np
 
 from keras_mml.utils.array.ternary_multiplication import ternary_multiplication
 
 
+def _array(a):
+    a = np.array(a)
+    if keras.config.backend() == "torch":
+        import torch
+
+        return torch.tensor(a)
+    return a
+
+
 def test_2d_multiplication():
     # Case 1
-    x = np.array([1.0, 2.0, 3.0])
-    w = np.array([[-1.0, 0, 1.0], [0, 1.0, 0], [1.0, 0, -1.0]]).T
+    x = _array([1.0, 2.0, 3.0])
+    w = _array([[-1.0, 0, 1.0], [0, 1.0, 0], [1.0, 0, -1.0]]).T
     scale = 2.0
 
     predicted = ternary_multiplication(x, w, scale)
-    correct = np.array([1.0, 1.0, -1.0])
+    correct = _array([1.0, 1.0, -1.0])
 
     assert np.allclose(predicted, correct)
 
     # Case 2
-    x = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
-    w = np.array([[-1.0, 0, 1.0, 0, 1.0], [0, 1.0, 0, -1.0, 0], [1.0, 0, -1.0, -1.0, 1.0]]).T
+    x = _array([1.0, 2.0, 3.0, 4.0, 5.0])
+    w = _array([[-1.0, 0, 1.0, 0, 1.0], [0, 1.0, 0, -1.0, 0], [1.0, 0, -1.0, -1.0, 1.0]]).T
     scale = 3.0
 
     predicted = ternary_multiplication(x, w, scale)
-    correct = np.array([7.0 / 3, -2.0 / 3, -1.0 / 3])
+    correct = _array([7.0 / 3, -2.0 / 3, -1.0 / 3])
 
     assert np.allclose(predicted, correct)
 
 
 def test_3d_multiplication():
     # Case 1
-    x = np.array([[1.0, 2.0, 3.0], [1.0, 3.0, 6.0]])
-    w = np.array([np.array([[-1.0, 0, 1.0], [0, 1.0, 0], [1.0, 0, -1.0]]).T])
+    x = _array([[1.0, 2.0, 3.0], [1.0, 3.0, 6.0]])
+    w = _array([np.array([[-1.0, 0, 1.0], [0, 1.0, 0], [1.0, 0, -1.0]]).T])
     scale = 2.0
 
     predicted = ternary_multiplication(x, w, scale)
-    correct = np.array([[[1.0, 1.0, -1.0], [2.5, 1.5, -2.5]]])
+    correct = _array([[[1.0, 1.0, -1.0], [2.5, 1.5, -2.5]]])
 
     assert np.allclose(predicted, correct)
 
     # Case 2
-    x = np.array([[1.0, 2.0, 3.0], [1.0, 3.0, 6.0]])
-    w = np.array(
+    x = _array([[1.0, 2.0, 3.0], [1.0, 3.0, 6.0]])
+    w = _array(
         [
             np.array([[-1.0, 0, 1.0], [0, 1.0, 0], [1.0, 0, -1.0]]).T,
             np.array([[1.0, 0, -1.0], [0, -1.0, 0], [-1.0, 0, 1.0]]).T,
@@ -47,22 +57,22 @@ def test_3d_multiplication():
     scale = 2.0
 
     predicted = ternary_multiplication(x, w, scale)
-    correct = np.array([[[1.0, 1.0, -1.0], [2.5, 1.5, -2.5]], [[-1.0, -1.0, 1.0], [-2.5, -1.5, 2.5]]])
+    correct = _array([[[1.0, 1.0, -1.0], [2.5, 1.5, -2.5]], [[-1.0, -1.0, 1.0], [-2.5, -1.5, 2.5]]])
 
     assert np.allclose(predicted, correct)
 
     # Case 3
-    x = np.array([[1.0, 2.0, 3.0, 4.0, 5.0]])
-    w = np.array([np.array([[-1.0, 0, 1.0, 0, 1.0], [0, 1.0, 0, -1.0, 0], [1.0, 0, -1.0, -1.0, 1.0]]).T])
+    x = _array([[1.0, 2.0, 3.0, 4.0, 5.0]])
+    w = _array([np.array([[-1.0, 0, 1.0, 0, 1.0], [0, 1.0, 0, -1.0, 0], [1.0, 0, -1.0, -1.0, 1.0]]).T])
     scale = 3.0
 
     predicted = ternary_multiplication(x, w, scale)
-    correct = np.array([[[7.0 / 3, -2.0 / 3, -1.0 / 3]]])
+    correct = _array([[[7.0 / 3, -2.0 / 3, -1.0 / 3]]])
     assert np.allclose(predicted, correct)
 
     # Case 4
-    x = np.array([[1.0, 2.0, 3.0, 4.0, 5.0]])
-    w = np.array(
+    x = _array([[1.0, 2.0, 3.0, 4.0, 5.0]])
+    w = _array(
         [
             np.array([[-1.0, 0, 1.0, 0, 1.0], [0, 1.0, 0, -1.0, 0], [1.0, 0, -1.0, -1.0, 1.0]]).T,
             np.array([[-1.0, 0, 0, 0, 0], [0, -1.0, 0, 0, 0], [0, 0, -1.0, 0, 0]]).T,
@@ -71,12 +81,12 @@ def test_3d_multiplication():
     scale = 3.0
 
     predicted = ternary_multiplication(x, w, scale)
-    correct = np.array([[[7.0 / 3, -2.0 / 3, -1.0 / 3]], [[-1.0 / 3, -2.0 / 3, -1.0]]])
+    correct = _array([[[7.0 / 3, -2.0 / 3, -1.0 / 3]], [[-1.0 / 3, -2.0 / 3, -1.0]]])
     assert np.allclose(predicted, correct)
 
     # Case 5
-    x = np.array([[1.0, 2.0, 3.0, 4.0, 5.0], [-1.0, 0.0, 1.0, -2.0, 2.0]])
-    w = np.array(
+    x = _array([[1.0, 2.0, 3.0, 4.0, 5.0], [-1.0, 0.0, 1.0, -2.0, 2.0]])
+    w = _array(
         [
             np.array([[-1.0, 0, 1.0, 0, 1.0], [0, 1.0, 0, -1.0, 0], [1.0, 0, -1.0, -1.0, 1.0]]).T,
             np.array([[-1.0, 0, 0, 0, 0], [0, -1.0, 0, 0, 0], [0, 0, -1.0, 0, 0]]).T,
@@ -85,7 +95,7 @@ def test_3d_multiplication():
     scale = 3.0
 
     predicted = ternary_multiplication(x, w, scale)
-    correct = np.array(
+    correct = _array(
         [
             [[7.0 / 3, -2.0 / 3, -1.0 / 3], [4.0 / 3, 2.0 / 3, 2.0 / 3]],
             [[-1.0 / 3, -2.0 / 3, -1.0], [1.0 / 3, 0, -1.0 / 3]],
