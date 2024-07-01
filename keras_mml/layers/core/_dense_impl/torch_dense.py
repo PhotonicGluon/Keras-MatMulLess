521ba33da37bcd1a7200d117418c769ae0efb64d
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-07-01 08:27:38 +0800

    Re-enable torch compile for `TorchDenseMML`

--- keras_mml/layers/core/_dense_impl/torch_dense.py
+++ keras_mml/layers/core/_dense_impl/torch_dense.py
@@ -9,7 +9,7 @@ import torch
 from keras_mml.layers.core._dense_impl.base_dense import EPSILON, BaseDenseMML
 
 
-# @torch.compile(mode="reduce-overhead")
+@torch.compile(mode="reduce-overhead")
 def _activations_quantization(x: torch.Tensor) -> torch.Tensor:
     """
     Quantizes the activations to 8-bit precision using absmax quantization.
@@ -26,7 +26,7 @@ def _activations_quantization(x: torch.Tensor) -> torch.Tensor:
     return y
 
 
-# @torch.compile(mode="reduce-overhead")
+@torch.compile(mode="reduce-overhead")
 def _compute_kernel_scale(w: torch.Tensor) -> float:
     """
     Computes the scale factor of the kernel matrix.
@@ -41,7 +41,7 @@ def _compute_kernel_scale(w: torch.Tensor) -> float:
     return 1.0 / torch.mean(torch.abs(w)).clamp_(EPSILON)
 
 
-# @torch.compile(mode="reduce-overhead")
+@torch.compile(mode="reduce-overhead")
 def _quantize_kernel(w: torch.Tensor, scale: float) -> torch.Tensor:
     """
     Quantizes the kernel values to 1.58 bits (i.e., :math:`\\log_{2}3` bits).
@@ -57,7 +57,7 @@ def _quantize_kernel(w: torch.Tensor, scale: float) -> torch.Tensor:
     return torch.clip(torch.round(w * scale), -1, 1)
 
 
-# @torch.compile(mode="reduce-overhead")
+@torch.compile(mode="reduce-overhead")
 def _get_x_quantized(x_norm: torch.Tensor) -> torch.Tensor:
     """
     Gets the quantized activations, with support for the backward direction by using STE gradient
@@ -75,7 +75,7 @@ def _get_x_quantized(x_norm: torch.Tensor) -> torch.Tensor:
     return x_norm + (_activations_quantization(x_norm) - x_norm).detach()
 
 
-# @torch.compile(mode="reduce-overhead")
+@torch.compile(mode="reduce-overhead")
 def _get_w_quantized(w: torch.Tensor, scale: float) -> torch.Tensor:
     """
     Gets the quantized kernel matrix, with support for the backward direction by using STE gradient
