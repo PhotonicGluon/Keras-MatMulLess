83f1e9fd24a5a01cfaeb9fb69441540cecf55a5f
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-07-02 08:57:55 +0800

    Add admonition that ternary multiplication falls back to matmul

--- keras_mml/utils/array/ternary_multiplication.py
+++ keras_mml/utils/array/ternary_multiplication.py
@@ -2,12 +2,19 @@
 Implementation of ternary multiplication.
 """
 
-from keras_mml.utils.array._ternary_multiplication_impl import backend_function
+from keras import ops
+
+# from keras_mml.utils.array._ternary_multiplication_impl import backend_function
 
 
 def ternary_multiplication(x_quantized, w_quantized, w_scale: float):
     """
     Applies the ternary multiplication algorithm.
+
+    .. IMPORTANT::
+        The current implementation of the ternary multiplication still falls back to ``ops.matmul``
+        in Keras, since the backend-dependent implementations seem to be slower than this baseline
+        function. This would be changed in a future version.
 
     Args:
         x_quantized: Quantized activation values.
@@ -18,4 +25,7 @@ def ternary_multiplication(x_quantized, w_quantized, w_scale: float):
         Multiplied matrix.
     """
 
-    return backend_function(x_quantized, w_quantized, w_scale)
+    # TODO: Optimize backend-dependent ternary multiplication
+    # return backend_function(x_quantized, w_quantized, w_scale)
+
+    return ops.matmul(x_quantized, w_quantized) / w_scale
