15da28974ec7a008582c98f93f931d4ccc220253
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-07-05 09:17:13 +0800

    Change alert to show on nightly as well

--- docs/source/_static/js/version-alert.js
+++ docs/source/_static/js/version-alert.js
@@ -2,29 +2,38 @@
 
 "use strict";
 
-// Adds admonition for the "latest" version -- which is the unreleased main branch.
-function warnOnLatestVersion() {
-    if (!window.READTHEDOCS_DATA || window.READTHEDOCS_DATA.version !== "latest") {
-        // Not on RTD, or not on latest
+// Adds admonition for the latest and nightly versions (i.e., the development versions)
+function warnOnDevelopmentVersions() {
+    if (
+        !(
+            window.READTHEDOCS_DATA &&
+            (window.READTHEDOCS_DATA.version === "latest" || window.READTHEDOCS_DATA.version === "nightly")
+        )
+    ) {
+        // Not on RTD, or not on one of the development versions
         return;
     }
 
     // Create the warning
     let warning = document.createElement("div");
     warning.setAttribute("class", "admonition danger");
-    warning.innerHTML = "<p class='first admonition-title'>Caution</p> " +
+    warning.innerHTML =
+        "<p class='first admonition-title'>Caution</p> " +
         "<p class='last'> " +
         "You're reading the documentation for a <strong>development version</strong>. " +
         "Documentation is available for the <a href='/en/stable/'>current stable release</a>, " +
         "or for older versions through the menu at bottom." +
         "</p>";
-    warning.querySelector("a").href = window.location.pathname.replace("/latest", "/stable");
+    warning.querySelector("a").href = window.location.pathname
+        .replace("/latest", "/stable")
+        .replace("/nightly", "/stable");
 
-    let parent = document.querySelector("div.article-container")
-        || document.querySelector("div.body")
-        || document.querySelector("div.document")
-        || document.body;
+    let parent =
+        document.querySelector("div.article-container") ||
+        document.querySelector("div.body") ||
+        document.querySelector("div.document") ||
+        document.body;
     parent.insertBefore(warning, parent.firstChild);
 }
 
-document.addEventListener("DOMContentLoaded", warnOnLatestVersion);
+document.addEventListener("DOMContentLoaded", warnOnDevelopmentVersions);
