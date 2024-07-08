bdb96d8c36f9942b93415dfe5e8ce58e8b00876c
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-07-08 14:55:46 +0800

    Remove `install.py` dependency on `click`

--- install.py
+++ install.py
@@ -5,31 +5,12 @@ Especially helpful during development.
 """
 
 import os
+from argparse import ArgumentParser
 from typing import Tuple
-
-import click
 
 REQUIREMENTS_FOLDER = "requirements"
 
 
-@click.command()
-@click.argument("groups", type=str, nargs=-1)
-@click.option(
-    "--backend",
-    "backends",
-    type=click.Choice(["tensorflow", "torch", "jax"]),
-    required=True,
-    multiple=True,
-    help="Backend dependencies to install.",
-)
-@click.option(
-    "--cuda",
-    "--with-cuda",
-    "with_cuda",
-    is_flag=True,
-    default=False,
-    help="Whether the installation is with CUDA support.",
-)
 def install(groups: Tuple[str, ...], backends: Tuple[str, ...], with_cuda: bool):
     """
     Dependencies installer.
@@ -48,8 +29,29 @@ def install(groups: Tuple[str, ...], backends: Tuple[str, ...], with_cuda: bool)
         requirements_path = os.path.join(REQUIREMENTS_FOLDER, requirements_subfolder, f"requirements-{backend}.txt")
         os.system(f"pip install -r {requirements_path}")
 
-    click.secho("Done!", fg="green")
+    print("\x1b[32mDone!")
 
 
 if __name__ == "__main__":
-    install()
+    parser = ArgumentParser(prog="install.py", description="Dependencies installer.")
+
+    parser.add_argument(
+        "groups",
+        nargs="+",
+        type=str.lower,
+        help="main dependency groups to install (from `pyproject.toml`)",
+    )
+    parser.add_argument(
+        "--backend",
+        nargs="*",
+        type=str.lower,
+        choices=["tensorflow", "torch", "jax"],
+        help="backend dependencies to install",
+    )
+    parser.add_argument(
+        "--with-cuda", "--cuda", action="store_true", help="whether the installation is with CUDA support"
+    )
+
+    args = parser.parse_args()
+
+    install(args.groups, args.backend if args.backend else [], args.with_cuda)
