656131c59d1462d29f6ca3ecfb65f8012ca0140d
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-07-08 13:18:43 +0800

    Add helpful note on which devcontainer is being used

--- .devcontainer/base/postCreateCommand.sh
+++ .devcontainer/base/postCreateCommand.sh
@@ -3,3 +3,12 @@ poetry install --with dev
 
 export temporary=$(poetry env info --path)
 echo "export PATH='$temporary/bin:$PATH'" >> ~/.bashrc
+
+# Tell user which devcontainer they are on
+echo "" >> ~/.bashrc  # Newline
+echo "# Notify that user is on CPU devcontainer" >> ~/.bashrc
+echo "echo '+---------------------------------+'" >> ~/.bashrc
+echo "echo '|                                 |'" >> ~/.bashrc
+echo "echo -e '| \x1b[34;3mYou are on the \x1b[34;1;4mCPU\x1b[0m\x1b[34;3m devcontainer\x1b[0m |'" >> ~/.bashrc
+echo "echo '|                                 |'" >> ~/.bashrc
+echo "echo '+---------------------------------+'" >> ~/.bashrc
--- .devcontainer/cuda/postCreateCommand.sh
+++ .devcontainer/cuda/postCreateCommand.sh
@@ -4,6 +4,15 @@ poetry install --with dev,nvidia-gpu  # ADDITIONAL: Need to install GPU dependen
 export temporary=$(poetry env info --path)
 echo "export PATH='$temporary/bin:$PATH'" >> ~/.bashrc
 
+# Tell user which devcontainer they are on
+echo "" >> ~/.bashrc  # Newline
+echo "# Notify that user is on CUDA devcontainer" >> ~/.bashrc
+echo "echo '+----------------------------------+'" >> ~/.bashrc
+echo "echo '|                                  |'" >> ~/.bashrc
+echo "echo -e '| \x1b[34;3mYou are on the \x1b[34;1;4mCUDA\x1b[0m\x1b[34;3m devcontainer\x1b[0m |'" >> ~/.bashrc
+echo "echo '|                                  |'" >> ~/.bashrc
+echo "echo '+----------------------------------+'" >> ~/.bashrc
+
 # ADDITIONAL: Set up CUDNN path
 CUDNN_PATH=$(dirname $(poetry run python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib" >> ~/.bashrc
