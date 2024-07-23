dfb61e55ef4097b1d40a407fc084c1e2e928a32e
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-07-23 08:12:40 +0800

    Forgot to add `QuantRMSNorm` to `layers` exports

--- keras_mml/layers/__init__.py
+++ keras_mml/layers/__init__.py
@@ -5,6 +5,6 @@ Layers implemented in Keras-MML.
 from .activations import GLUMML, BilinearMML, GeGLUMML, ReGLUMML, SeGLUMML, SwiGLUMML
 from .core import DenseMML, PatchEmbedding, TokenEmbedding
 from .misc import Patches
-from .normalizations import RMSNorm
+from .normalizations import QuantRMSNorm, RMSNorm
 from .recurrent import GRUMML, LRUMML, GRUCellMML, LRUCellMML
 from .transformer import AttentionMML, TransformerBlockMML
