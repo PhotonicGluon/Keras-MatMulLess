dd49a2a32b7ff64d3f5f3ccbe4e39827ec372399
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2024-08-19 15:59:10 +0800

    Bump package versions

--- keras_mml/__init__.py
+++ keras_mml/__init__.py
@@ -4,5 +4,7 @@ Keras layers without using matrix multiplications.
 
 from . import layers
 
-__version__ = "0.1.0"
-__version_tuple__ = (0, 1, 0)
+__version__ = "0.1.1"
+__version_tuple__ = (0, 1, 1)
+
+__all__ = ["layers"]
